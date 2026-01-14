6c66208511a2dd4b95efd6af0470a2c1254017a2
shirwani <zakishirwani@alumni.purdue.edu>
2026-01-13 19:45:05 -0500

    doit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def run():
     return render_template("index.html",
-                           image_path='https://debian-us-east-zakishirwani.com.us-east-1.linodeobjects.com/puntacana/puntacana.jpg',
+                           image_path='https://zakishirwani.com.nyc3.cdn.digitaloceanspaces.com',
                            js_path='static/js/script.js')
 
 if __name__ == '__main__':
